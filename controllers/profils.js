7259f8948ff908f45ee776ffd06bc84b61abc0fd
inno bdx <inno.bdx@proton.me>
2023-06-06 14:46:27 +0000

    Add compte opensoum

--- controllers/profils.js
+++ controllers/profils.js
@@ -7,5 +7,5 @@ module.exports = [
     { _id: 4, nom: "Admin", menus: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13] },
     { _id: 5, nom: "Torréfacteur", menus: [1, 9, 11] },
     { _id: 6, nom: "Torréfacteur Admin", menus: [1, 9, 12] },
-
+    { _id: 7, nom: "OpenSoum", menus: [1, 3, 4] },
 ];
