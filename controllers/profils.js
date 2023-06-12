1daa6f6e655b9e45320eef30c93ddb00427e432c
Laurent <laurent.cuadras@gmail.com>
2023-06-12 10:40:58 +0200

    maj bdd profils

--- controllers/profils.js
+++ controllers/profils.js
@@ -1,11 +1,9 @@
 'use strict';
 
 module.exports = [
-    { _id: 1, nom: "Client", menus: [1, 2, 9] },
-    { _id: 2, nom: "Barman", menus: [1, 2, 3, 4, 5, 9] },
-    { _id: 3, nom: "Gestionnaire", menus: [1, 2, 6, 7, 8, 9, 13] },
-    { _id: 4, nom: "Admin", menus: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13] },
-    { _id: 5, nom: "Torréfacteur", menus: [1, 9, 11] },
-    { _id: 6, nom: "Torréfacteur Admin", menus: [1, 9, 12] },
-    { _id: 7, nom: "OpenSoum", menus: [1, 3, 4] },
+    { _id: 1, nom: "Client", menus: [1, 2] },
+    { _id: 2, nom: "Barman", menus: [1, 2, 3, 4, 5] },
+    { _id: 3, nom: "Gestionnaire", menus: [1, 2, 6, 7, 8, 9] },
+    { _id: 4, nom: "Admin", menus: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
+    { _id: 5, nom: "OpenSoum", menus: [1, 3, 4] },
 ];
